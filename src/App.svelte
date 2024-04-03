9aca30ae0055b8c4aa39a200b04d02c580765b4b
mythuscry <mythuscry@gmail.com>
2024-04-03 10:41:29 +0800

    1.创建sc2tools文件夹

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Greet from './lib/Map.svelte'
+  import Draw from './lib/draw.svelte';
+  import Extend from './lib/extend.svelte';
+</script>
+
+<main class="container">
+  <div class="row">
+    <Extend/>
+    <Greet />
+    <Draw/>
+  </div>
+
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/extend.svelte
@@ -0,0 +1,4 @@
+<script></script>
+<div>
+    <h1>extend</h1>
+</div>
